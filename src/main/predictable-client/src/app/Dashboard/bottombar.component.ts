c253fa0e9eb7ce7f6b20dfe3dd81361bf930c8b3
Jai <jaikumar.bhagwani@gmail.com>
2018-09-14 17:05:36 +0530

    New UI integrated

--- src/main/predictable-client/src/app/Dashboard/bottombar.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector:'bottom-toolbar',
-    templateUrl:"bottombar.component.html"
-})
-export class BottomToolbarComponent{}
--- src/main/predictable-client/src/app/Services/product.service.ts
+++ src/main/predictable-client/src/app/Services/product.service.ts
@@ -3,26 +3,30 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { IProduct } from '../Models/productModel';
+import "rxjs/add/operator/map"
+
 
 @Injectable()
 export class ProductService{
     constructor(private http: HttpClient) {}
 
-    getProducts():IProduct[]{
-        return [
-            {id:1,name:"Product 1", companyId:1,companyVO:null},
-            {id:2,name:"Product 2", companyId:2,companyVO:null},
-            {id:3,name:"Product 3", companyId:3,companyVO:null},
-            {id:4,name:"Product 4", companyId:4,companyVO:null},
-            {id:5,name:"Product 5", companyId:4,companyVO:null},
-            {id:6,name:"Product 6", companyId:4,companyVO:null},
-            {id:7,name:"Product 7", companyId:4,companyVO:null},
-            {id:8,name:"Product 8", companyId:4,companyVO:null},
-            {id:9,name:"Product 9", companyId:4,companyVO:null},
-            {id:10,name:"Product 10", companyId:4,companyVO:null},
-            {id:11,name:"Product 11", companyId:4,companyVO:null},
-            {id:12,name:"Product 12", companyId:4,companyVO:null},
-        ];
+    getProducts(): Observable<IProduct[]>{
+        // return [
+        //     {id:1,name:"Product 1", companyId:1,companyVO:null},
+        //     {id:2,name:"Product 2", companyId:2,companyVO:null},
+        //     {id:3,name:"Product 3", companyId:3,companyVO:null},
+        //     {id:4,name:"Product 4", companyId:4,companyVO:null},
+        //     {id:5,name:"Product 5", companyId:4,companyVO:null},
+        //     {id:6,name:"Product 6", companyId:4,companyVO:null},
+        //     {id:7,name:"Product 7", companyId:4,companyVO:null},
+        //     {id:8,name:"Product 8", companyId:4,companyVO:null},
+        //     {id:9,name:"Product 9", companyId:4,companyVO:null},
+        //     {id:10,name:"Product 10", companyId:4,companyVO:null},
+        //     {id:11,name:"Product 11", companyId:4,companyVO:null},
+        //     {id:12,name:"Product 12", companyId:4,companyVO:null},
+        // ];
+
+        return this.http.get<IProduct[]>('product/all?companyId=1');
     }
 
     saveProduct(product:IProduct):boolean{
