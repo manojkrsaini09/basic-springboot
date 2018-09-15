72f1c9f0245b358c66cf890cadf73368341e255e
Jai <jaikumar.bhagwani@gmail.com>
2018-09-15 11:39:27 +0530

    product service changes

--- src/main/predictable-client/src/app/Services/product.service.ts
+++ src/main/predictable-client/src/app/Services/product.service.ts
@@ -8,37 +8,30 @@ import "rxjs/add/operator/map"
 
 @Injectable()
 export class ProductService{
+    errorMessage: string;
+
     constructor(private http: HttpClient) {}
 
-    getProducts(): Observable<IProduct[]>{
-        // return [
-        //     {id:1,name:"Product 1", companyId:1,companyVO:null},
-        //     {id:2,name:"Product 2", companyId:2,companyVO:null},
-        //     {id:3,name:"Product 3", companyId:3,companyVO:null},
-        //     {id:4,name:"Product 4", companyId:4,companyVO:null},
-        //     {id:5,name:"Product 5", companyId:4,companyVO:null},
-        //     {id:6,name:"Product 6", companyId:4,companyVO:null},
-        //     {id:7,name:"Product 7", companyId:4,companyVO:null},
-        //     {id:8,name:"Product 8", companyId:4,companyVO:null},
-        //     {id:9,name:"Product 9", companyId:4,companyVO:null},
-        //     {id:10,name:"Product 10", companyId:4,companyVO:null},
-        //     {id:11,name:"Product 11", companyId:4,companyVO:null},
-        //     {id:12,name:"Product 12", companyId:4,companyVO:null},
-        // ];
-
-        return this.http.get<IProduct[]>('product/all?companyId=50');
+    getProducts(orgnizationId: number): Observable<IProduct[]>{
+        if(orgnizationId > 0){
+            return this.http.get<IProduct[]>('product/all?companyId='+orgnizationId);
+        }
+        else{
+            return this.http.get<IProduct[]>('product/all?companyId=');
+        }
     }
 
-    saveProduct(product:IProduct):boolean{
-        return true;
+    saveProduct(product:IProduct):Observable<IProduct>{
+        return this.http.post<IProduct>('product/create', product)
     }
 
-    deleteProduct(id:number):boolean{
-        return true;
+    deleteProduct(id:number):Observable<IProduct>{
+        return this.http.get<IProduct>('product/delete?id='+id);
     }
 
-    updateProduct(product:IProduct):boolean{
-        return true;
+
+    updateProduct(product:IProduct):Observable<IProduct>{
+        return this.http.post<IProduct>('product/update', product)
     }
 
 }
